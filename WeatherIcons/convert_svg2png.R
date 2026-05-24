c879f37e142f2cfbfcd95dadc27a535ea315ae98
Karlduino <karlduino@gmail.com>
2026-05-24 07:56:17 -0500

    convert_svg2png.R: add PNG/ dir if needed
    also change "=" to "<-"

--- WeatherIcons/convert_svg2png.R
+++ WeatherIcons/convert_svg2png.R
@@ -1,16 +1,20 @@
 #!/usr/bin/Rscript
 
 # replace spaces with _
-files = list.files("SVG")
+files <- list.files("SVG")
 for(file in files) {
     if(grepl("\\s+", file)) # if white space, replace with _
         system(paste0('replace_spaces.rb "', file.path("SVG", file), '"'))
 }
 
+# if PNG directory doesn't exist, create
+png_dir <- "PNG"
+if(!dir.exists(png_dir)) dir.create(png_dir)
+
 # convert SVG -> PNG
-files = list.files("SVG")
+files <- list.files("SVG")
 for(file in files) {
-    ifile = file.path("SVG", file)
-    ofile = file.path("PNG", sub("\\.svg$", ".png", file))
+    ifile <- file.path("SVG", file)
+    ofile <- file.path("PNG", sub("\\.svg$", ".png", file))
     system(paste('convert', ifile, ofile))
 }
