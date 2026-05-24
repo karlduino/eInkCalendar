723370e957ed04de2aa4582695dc36c9021032a4
Karlduino <karlduino@gmail.com>
2026-05-24 08:40:28 -0500

    scripts/get_weather.py: attempting to get air quality info

--- scripts/get_weather.py
+++ scripts/get_weather.py
@@ -27,7 +27,9 @@ def main():
     url = "https://api.openweathermap.org/data/2.5/weather"
     weather_json["units"] = "imperial"
 
+    # get weather datas
     response = requests.post(url, params=weather_json)
+    data = {}
     if response.status_code == 200:
         data = response.json()
 
@@ -38,6 +40,32 @@ def main():
         print("icon file:  ", icons[data['weather'][0]['icon']])
         print("sunrise:    ", convert_date(data['sys']['sunrise']))
         print("sunset:     ", convert_date(data['sys']['sunset']))
+
+    else:
+        print("weather error ", response.status_code)
+
+    # api call for air pollution
+    aqi_url = "https://api.openweathermap.org/data/2.5/air_pollution"
+    del weather_json["units"]
+
+    air_quality = {"1": "good",
+                   "2": "fair",
+                   "3": "moderate",
+                   "4": "poor",
+                   "5": "very poor"}
+
+    # get air pollution data
+    aqi_response = requests.post(aqi_url, params=weather_json)
+    aqi_data = {}
+    if aqi_response.status_code == 200:
+        aqi_data = response.json()
+
+        print("aqi:        ", air_quality[aqi_data['list']['main']['aqi']])
+
+    else:
+        print("aqi error ", aqi_response.status_code)
+
+
 
 
 def convert_date(ts, format="%I:%M %p"):
