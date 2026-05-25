c9921ccf2722a1e684efa5b08570a796f7537ead
Karlduino <karlduino@gmail.com>
2026-05-25 15:27:24 -0500

    combine_images.sh: script to combine images for 800x480 background

---
+++ WeatherIcons/combine_images.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+convert BMPsmall/DAY_snow.bmp -resize 50x50 tmp1.bmp
+convert tmp1.bmp -background white -gravity northeast -splice 375x10 tmp2.bmp
+convert tmp2.bmp -background white -gravity southwest -splice 375x420 tmp3.bmp
+
+convert BMPsmall/Sunrise.bmp -resize 30x30 tmp4.bmp
+convert BMPsmall/Sunset.bmp -resize 30x30 tmp5.bmp
+convert tmp3.bmp tmp4.bmp -gravity northwest -geometry 30x30+600+20 -composite tmp6.bmp
+convert tmp6.bmp tmp5.bmp -gravity northwest -geometry 30x30+700+20 -composite result.bmp
+rm tmp?.bmp
