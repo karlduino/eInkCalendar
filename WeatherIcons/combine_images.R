cd215488679af8de13e55db4727a25e3a0c0cde7
Karlduino <karlduino@gmail.com>
2026-06-14 08:06:12 -0500

    Revise code to make background images; and pngs to adjust colors

--- WeatherIcons/combine_images.R
+++ WeatherIcons/combine_images.R
@@ -16,10 +16,11 @@ for(file in files) {
    cat(file, "\n")
    system(paste("convert ", file.path(idir, file), "-resize 50x50 tmp1.bmp"))
    system("convert tmp1.bmp -background white -gravity northeast -splice 375x0 tmp2.bmp")
-   system("convert tmp2.bmp -background white -gravity southwest -splice 375x415 tmp3.bmp")
+   system("convert tmp2.bmp -background white -gravity southwest -splice 375x430 tmp3.bmp")
 
    system("convert tmp3.bmp tmp4.bmp -gravity northwest -geometry 25x25+320+40 -composite tmp6.bmp")
-   system(paste("convert tmp6.bmp tmp5.bmp -gravity northwest -geometry 25x25+410+40 -composite", file.path(odir, file)))
+   system(paste("convert tmp6.bmp tmp5.bmp -gravity northwest -geometry 25x25+410+40 -composite tmp7.bmp"))
+   system(paste0("convert tmp7.bmp -dither FloydSteinberg -define dither:diffusion-amount=85% -remap eink_png/epaper_eink-2color.png BMP3:", file.path(odir, file)))
 }
 
 unlink("tmp*.bmp")
