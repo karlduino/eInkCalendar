b80a780bb95edc05edeeade6fb6e9080f8399b7c
Karlduino <karlduino@gmail.com>
2026-06-14 08:14:30 -0500

    combine_images.R: adjust size and position of weather icons

--- WeatherIcons/combine_images.R
+++ WeatherIcons/combine_images.R
@@ -7,19 +7,26 @@ if(!dir.exists(odir)) dir.create(odir)
 
 files <- list.files(idir)
 
-system("convert BMP/Sunrise.bmp -resize 25x25 tmp4.bmp")
-system("convert BMP/Sunset.bmp -resize 25x25 tmp5.bmp")
+system("convert BMP/Sunrise.bmp -resize 32x32 tmp4.bmp")
+system("convert BMP/Sunset.bmp -resize 32x32 tmp5.bmp")
 
 for(file in files) {
    if(!grepl("^DAY", file) && !grepl("^NIGHT", file)) next
 
    cat(file, "\n")
-   system(paste("convert ", file.path(idir, file), "-resize 50x50 tmp1.bmp"))
-   system("convert tmp1.bmp -background white -gravity northeast -splice 375x0 tmp2.bmp")
-   system("convert tmp2.bmp -background white -gravity southwest -splice 375x430 tmp3.bmp")
 
-   system("convert tmp3.bmp tmp4.bmp -gravity northwest -geometry 25x25+320+40 -composite tmp6.bmp")
-   system(paste("convert tmp6.bmp tmp5.bmp -gravity northwest -geometry 25x25+410+40 -composite tmp7.bmp"))
+   # reside image for today's weather
+   system(paste("convert ", file.path(idir, file), "-resize 56x56 tmp1.bmp"))
+
+   # fill out to 800x480
+   system("convert tmp1.bmp -background white -gravity northeast -splice 372x0 tmp2.bmp")
+   system("convert tmp2.bmp -background white -gravity southwest -splice 372x424 tmp3.bmp")
+
+   # merge in sunrise/sunset icons
+   system("convert tmp3.bmp tmp4.bmp -gravity northwest -geometry 25x25+320+48 -composite tmp6.bmp")
+   system(paste("convert tmp6.bmp tmp5.bmp -gravity northwest -geometry 25x25+410+48 -composite tmp7.bmp"))
+
+   # convert colorscale
    system(paste0("convert tmp7.bmp -dither FloydSteinberg -define dither:diffusion-amount=85% -remap eink_png/epaper_eink-2color.png BMP3:", file.path(odir, file)))
 }
 
